b164b3f47e15ce5f461334babfa90e5f958d08d2
KotaniKazumi <kkotani@gol.com>
2021-09-15 16:54:17 +0900

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# firstprj
+# django ログプログラム
+#       by kazumi kotani 2021.9.15
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView, DetailView
 from .models import BlogPost
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from .forms import ContactForm
+from django.contrib import messages
 
 class IndexView(ListView):
 	template_name = 'index.html'
@@ -34,7 +38,16 @@ class MusicView(ListView):
 	context_object_name = 'music_records'
 	queryset = BlogPost.objects.filter(category='music').order_by('-posted_at')
 	paginate_by = 2
-
+	
+class ContactView(FormView):
+	template_name ='contact.html'
+	form_class = ContactForm
+	success_url = reverse_lazy('blog:contact')
+	
+	def form_valid(self, form):
+		form.send_email()
+		messages.success(self.request,'お問い合わせは正常に送信されました。')
+		return super().form_valid(form)
 
 '''	
 	def get_context_data(self, **kwargs):
